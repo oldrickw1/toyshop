d9221aec7a2c5fb460128a8a63084b3c85b9c9da
Oldrick Weenink <o.weenink95@hotmail.com>
2024-04-20 17:37:06 +0200

    add random sentence feature

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function showAlert() {
     alert('this works');
 };
+
+function showText() {
+    document.querySelector("#placeholder").innerText = randomTexts[Math.round(Math.random(4)*10)];
+}
+
+const randomTexts = [
+  "The quick brown fox jumps over the lazy dog.",
+  "She sells seashells by the seashore.",
+  "How much wood would a woodchuck chuck if a woodchuck could chuck wood?",
+  "An apple a day keeps the doctor away.",
+  "A watched pot never boils.",
+  "Actions speak louder than words.",
+  "Beauty is in the eye of the beholder.",
+  "Don't cry over spilled milk.",
+  "Early to bed and early to rise, makes a man healthy, wealthy, and wise.",
+  "Every cloud has a silver lining."
+];
